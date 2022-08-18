1b73b97108401cf1b51aeac3887664af08d6b75b
Bogdan <bogd@live.ru>
2022-08-18 12:19:51 +0200

    rollback web interface for debug, components update

--- main/main.c
+++ main/main.c
@@ -22,63 +22,9 @@ HTTP_IO_RESULT AfterPostHandlerCustom(httpd_req_t *req, const char *filename, ch
 void app_main(void)
 {
 
-    esp_err_t err = nvs_flash_init();
-    ESP_ERROR_CHECK(esp_netif_init());
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
-    if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND || MANUAL_RESET == 1)
-    {
-        // 1.OTA app partition table has a smaller NVS partition size than the non-OTA
-        // partition table. This size mismatch may cause NVS initialization to fail.
-        // 2.NVS partition contains data in new format and cannot be recognized by this version of code.
-        // If this happens, we erase NVS partition and initialize NVS again.
-        ESP_ERROR_CHECK(nvs_flash_erase());
-        ESP_ERROR_CHECK(nvs_flash_init());
-        ESP_ERROR_CHECK(ResetInitSysConfig());
-    }
-    ESP_ERROR_CHECK(InitSysConfig());
+    WebGuiAppInit();
 
-    //init rom file system
-    init_rom_fs("/espfs");
 
-#if CONFIG_WEBGUIAPP_GPRS_ENABLE
-    /*Start PPP modem*/
-    if (GetSysConf()->gsmSettings.Flags1.bIsGSMEnabled)
-        PPPModemStart();
-#endif
-
-#if CONFIG_WEBGUIAPP_ETHERNET_ENABLE
-    /*Start Ethernet connection*/
-    if (GetSysConf()->ethSettings.Flags1.bIsETHEnabled)
-        EthStart();
-#endif
-
-#if CONFIG_WEBGUIAPP_WIFI_ENABLE
-    /*Start WiFi connection*/
-    if (GetSysConf()->wifiSettings.Flags1.bIsWiFiEnabled)
-    {
-        if (GetSysConf()->wifiSettings.Flags1.bIsAP)
-            WiFiAPStart();
-        else
-            WiFiSTAStart();
-    }
-#endif
-
-    /*Start time synchronization*/
-    if (CONFIG_WEBGUIAPP_GPRS_ENABLE ||
-    CONFIG_WEBGUIAPP_ETHERNET_ENABLE ||
-            (CONFIG_WEBGUIAPP_WIFI_ENABLE && GetSysConf()->wifiSettings.Flags1.bIsAP))
-    {
-        StartTimeGet();
-
-#if CONFIG_WEBGUIAPP_MQTT_ENABLE
-        if (GetSysConf()->mqttStation[0].Flags1.bIsGlobalEnabled
-                || GetSysConf()->mqttStation[1].Flags1.bIsGlobalEnabled)
-        {
-            MQTTRun();
-        }
-#endif
-
-    }
 
     /*Start web server*/
     if (CONFIG_WEBGUIAPP_GPRS_ENABLE ||
@@ -93,6 +39,6 @@ void app_main(void)
     while (true)
     {
         printf("Hello from app_main!\n");
-        sleep(1);
+        sleep(5);
     }
 }

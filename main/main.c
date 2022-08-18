cadd90a3ca173cd1b0c09c279ecf10a8f650f75f
Bogdan <bogd@live.ru>
2022-08-18 18:11:18 +0200

    remove http server start from user application

--- main/main.c
+++ main/main.c
@@ -1,17 +1,10 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <unistd.h>
-#include "freertos/FreeRTOS.h"
-#include "esp_event.h"
-#include "esp_netif.h"
-#include "nvs_flash.h"
 #include "esp_system.h"
-#include "driver/gpio.h"
 #include "sdkconfig.h"
 
 #include "SystemConfiguration.h"
-#include "romfs.h"
-#include "NetTransport.h"
 #include "HTTPServer.h"
 
 #define MANUAL_RESET 0
@@ -23,22 +16,11 @@ void app_main(void)
 {
 
     WebGuiAppInit();
+    regHTTPPrintCustom(&HTTPPrintCustom);
+    regAfterPostHandlerCustom(&AfterPostHandlerCustom);
 
-
-
-    /*Start web server*/
-    if (CONFIG_WEBGUIAPP_GPRS_ENABLE ||
-    CONFIG_WEBGUIAPP_ETHERNET_ENABLE ||
-    CONFIG_WEBGUIAPP_WIFI_ENABLE)
-    {
-        ESP_ERROR_CHECK(start_file_server());
-        regHTTPPrintCustom(&HTTPPrintCustom);
-        regAfterPostHandlerCustom(&AfterPostHandlerCustom);
-
-    }
     while (true)
     {
-        printf("Hello from app_main!\n");
-        sleep(5);
+        sleep(1);
     }
 }

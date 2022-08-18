c878051fe070c5207d3c6278a4634b3305de96b8
Bogdan <bogd@live.ru>
2022-08-18 11:07:39 +0200

    Post user implementation added

--- main/main.c
+++ main/main.c
@@ -17,6 +17,7 @@
 #define MANUAL_RESET 0
 
 int HTTPPrintCustom(httpd_req_t *req, char *buf, char *var);
+HTTP_IO_RESULT AfterPostHandlerCustom(httpd_req_t *req, const char *filename, char *PostData);
 
 void app_main(void)
 {
@@ -86,6 +87,7 @@ void app_main(void)
     {
         ESP_ERROR_CHECK(start_file_server());
         regHTTPPrintCustom(&HTTPPrintCustom);
+        regAfterPostHandlerCustom(&AfterPostHandlerCustom);
 
     }
     while (true)

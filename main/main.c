b64880b57ab8bdb5157d8c2ba725fb61ca761a73
Bogdan <bogd@live.ru>
2022-08-19 10:00:28 +0200

    update component, added example mqtt user data handler

--- main/main.c
+++ main/main.c
@@ -6,11 +6,13 @@
 
 #include "SystemConfiguration.h"
 #include "HTTPServer.h"
+#include "MQTT.h"
 
 #define MANUAL_RESET 0
 
 int HTTPPrintCustom(httpd_req_t *req, char *buf, char *var);
 HTTP_IO_RESULT AfterPostHandlerCustom(httpd_req_t *req, const char *filename, char *PostData);
+void UserDataHndlr(char *data, uint32_t len, int idx);
 
 void app_main(void)
 {
@@ -18,6 +20,8 @@ void app_main(void)
     WebGuiAppInit();
     regHTTPPrintCustom(&HTTPPrintCustom);
     regAfterPostHandlerCustom(&AfterPostHandlerCustom);
+    regUserDataHandler(&UserDataHndlr);
+
 
     while (true)
     {

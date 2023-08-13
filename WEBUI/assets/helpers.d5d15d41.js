fd21f72fc995afa93d52fc3e0be5bc9f03ebf099
bogdan <bogd@live.ru>
2023-08-13 18:35:45 +0200

    updated with latest web ui

--- WEBUI/assets/helpers.d5d15d41.js
+++
@@ -1,1 +0,0 @@
-import{u as f}from"./network.88ca996f.js";f();function e(r){r=Number(r);var t=Math.floor(r/86400),o=Math.floor(r%86400/3600),a=Math.floor(r%3600/60),s=Math.floor(r%3600%60);return" "+t+"d "+(o<10?"0":"")+o+":"+(a<10?"0":"")+a+":"+(s<10?"0":"")+s}export{e as s};

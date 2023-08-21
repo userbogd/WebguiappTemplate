97bbf878e1e80fe4a1572fac250a8b17e04e9452
Bogdan Pilyugin <bogd@live.ru>
2023-08-21 11:31:27 +0200

    implemented OTA firmware update

--- WEBUI_SRC/build_ui.bat
+++ WEBUI_SRC/build_ui.bat
@@ -1,12 +1,12 @@
-@ECHO ON
+@ECHO OFF
 SETLOCAL
-SET "sourcedir=C:\BOGD-PROJECTS\WebguiappTemplate\WEBUI"
+SET "sourcedir=C:\BOGD-PROJECTS\WebguiappTemplate\WEBUI_DIST"
 SET "keepfile=espfs.paths"
 SET "keepdir=keep"
 
 FOR /d %%a IN ("%sourcedir%\*") DO IF /i NOT "%%~nxa"=="%keepdir%" RD /S /Q "%%a"
 FOR %%a IN ("%sourcedir%\*") DO IF /i NOT "%%~nxa"=="%keepfile%" DEL "%%a
-xcopy /s "C:\BOGD-PROJECTS\webui\dist\spa" "C:\BOGD-PROJECTS\WebguiappTemplate\WEBUI"
+xcopy /s "C:\BOGD-PROJECTS\WebguiappTemplate\WEBUI_SRC\dist\spa" "C:\BOGD-PROJECTS\WebguiappTemplate\WEBUI_DIST"
 
 
 

d32d74334248154604ee5b22bcf6fa4b306c9aec
Bogdan Pilyugin <bogd@live.ru>
2023-08-21 12:22:44 +0200

    updated windows pre build script

--- WEBUI_SRC/build_ui.bat
+++ WEBUI_SRC/build_ui.bat
@@ -1,14 +1,9 @@
-@ECHO OFF
+@ECHO ON
 SETLOCAL
-SET "sourcedir=C:\BOGD-PROJECTS\WebguiappTemplate\WEBUI_DIST"
+SET "sourcedir=..\WEBUI_DIST"
 SET "keepfile=espfs.paths"
 SET "keepdir=keep"
 
 FOR /d %%a IN ("%sourcedir%\*") DO IF /i NOT "%%~nxa"=="%keepdir%" RD /S /Q "%%a"
 FOR %%a IN ("%sourcedir%\*") DO IF /i NOT "%%~nxa"=="%keepfile%" DEL "%%a
-xcopy /s "C:\BOGD-PROJECTS\WebguiappTemplate\WEBUI_SRC\dist\spa" "C:\BOGD-PROJECTS\WebguiappTemplate\WEBUI_DIST"
-
-
-
-
-
+xcopy /s /e .\dist\spa  %sourcedir%

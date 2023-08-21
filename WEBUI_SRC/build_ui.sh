6def55bd3b57730f380e06fa50a9d52625f307d7
bogdan <bogd@live.ru>
2023-08-21 13:00:40 +0200

    updated linux prebuild script

--- WEBUI_SRC/build_ui.sh
+++ WEBUI_SRC/build_ui.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 quasar b
-cd /home/bogdan/BOGD_PROJECTS/WebguiappTemplate/WEBUI_DIST
+cd ../WEBUI_DIST
 ls | grep -xv "espfs.paths" | xargs rm -r
-cp -r /home/bogdan/BOGD_PROJECTS/WebguiappTemplate/WEBUI_SRC/dist/spa/. /home/bogdan/BOGD_PROJECTS/WebguiappTemplate/WEBUI_DIST/
+cd ../WEBUI_SRC
+cp -r ./dist/spa/. ../WEBUI_DIST/

d86c632b665878b18d21c4b6ffc9b4a8e7e48687
bogdan <bogd@live.ru>
2023-09-23 16:05:29 +0200

    removed ttn-esp32

--- main/main.c
+++ main/main.c
@@ -10,7 +10,7 @@
 int HTTPPrintCustom(httpd_req_t *req, char *buf, char *var, int arg);
 HTTP_IO_RESULT AfterPostHandlerCustom(httpd_req_t *req, const char *filename, char *PostData);
 void UserMQTTEventHndlr(int idx, void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data);
-void TimeObtainHandler(struct timeval* tm);
+void TimeObtainHandler(struct timeval *tm);
 void SaveUserConf();
 
 const char my_context_data[] = "MyContextDataPassedIntoMQTTHandler";
@@ -19,12 +19,11 @@ void app_main(void)
 {
     regHTTPPrintCustom(&HTTPPrintCustom);
     regAfterPostHandlerCustom(&AfterPostHandlerCustom);
-    regUserEventHandler(&UserMQTTEventHndlr, (void*)my_context_data);
+    regUserEventHandler(&UserMQTTEventHndlr, (void*) my_context_data);
     regTimeSyncCallback(&TimeObtainHandler);
     regCustomSaveConf(&SaveUserConf);
 
     RegAppVariables();
-
 
     WebGuiAppInit();
     if (GetUserAppNeedReset())
@@ -33,7 +32,6 @@ void app_main(void)
         ESP_ERROR_CHECK(InitAppConfig());
     }
     ESP_ERROR_CHECK(InitAppConfig());
-
 
     while (true)
     {

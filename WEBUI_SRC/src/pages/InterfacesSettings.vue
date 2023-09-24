1c0d656ba064c25096dc13e86322f8434211868e
bogdan <bogd@live.ru>
2023-09-24 10:24:42 +0200

    rarely used components added as idf dependencies

--- WEBUI_SRC/src/pages/InterfacesSettings.vue
+++ WEBUI_SRC/src/pages/InterfacesSettings.vue
@@ -2,12 +2,16 @@
   <div class="cardholder">
     <WifiSetCard></WifiSetCard>
     <EthSetCard></EthSetCard>
+    <GSMSetCard></GSMSetCard>
+    <LoRaSetCard></LoRaSetCard>
   </div>
 </template>
 
 <script setup>
 import WifiSetCard from "components/webguicomp/cards/WifiSetCard.vue";
 import EthSetCard from "components/webguicomp/cards/EthSetCard.vue";
+import GSMSetCard from "src/components/webguicomp/cards/GSMSetCard.vue";
+import LoRaSetCard from "src/components/webguicomp/cards/LoRaSetCard.vue";
 
 defineOptions({
   name: 'InterfacesSettings'

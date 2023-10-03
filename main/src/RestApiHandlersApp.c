97fb6e44c3f6ce10da3007dc85d21fd175ae9565
bogdan <bogd@live.ru>
2023-10-03 17:05:27 +0200

    CRON API response implemented

--- main/src/RestApiHandlersApp.c
+++ main/src/RestApiHandlersApp.c
@@ -1,4 +1,4 @@
- /*! Copyright 2023 Bogdan Pilyugin
+/*! Copyright 2023 Bogdan Pilyugin
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,8 +23,56 @@
 
 #include "webguiapp.h"
 #include "AppConfiguration.h"
+#include "CronTimers.h"
 
 extern APP_CONFIG AppConfig;
+
+static void funct_cronrecs(char *argres, int rw)
+{
+    struct jWriteControl jwc;
+    jwOpen(&jwc, argres, VAR_MAX_VALUE_LENGTH, JW_ARRAY, JW_COMPACT);
+    for (int idx = 0; idx < CRON_TIMERS_NUMBER; idx++)
+    {
+        cron_timer_t T;
+        memcpy(&T, &GetAppConf()->Timers[idx], sizeof(cron_timer_t));
+        jwArr_object(&jwc);
+        jwObj_int(&jwc, "num", (unsigned int) T.num);
+        jwObj_int(&jwc, "del", (T.del) ? 1 : 0);
+        jwObj_int(&jwc, "enab", (T.enab) ? 1 : 0);
+        jwObj_int(&jwc, "prev", (T.prev) ? 1 : 0);
+        jwObj_string(&jwc, "name", T.name);
+        jwObj_int(&jwc, "obj", (unsigned int) T.obj);
+        jwObj_int(&jwc, "act", (unsigned int) T.act);
+        jwObj_string(&jwc, "cron", T.cron);
+        jwEnd(&jwc);
+    }
+    jwClose(&jwc);
+}
+
+static void funct_cronobjs(char *argres, int rw)
+{
+    struct jWriteControl jwc;
+    jwOpen(&jwc, argres, VAR_MAX_VALUE_LENGTH, JW_ARRAY, JW_COMPACT);
+    for (int idx = 0; idx < CRON_OBJECTS_NUMBER; idx++)
+    {
+        jwArr_object(&jwc);
+        jwObj_string(&jwc, "name", GetAppConf()->CronObjects[idx].objname);
+        jwObj_raw(&jwc, "acts", GetCronActAvail(idx));
+        jwEnd(&jwc);
+    }
+    jwClose(&jwc);
+}
+
+static void funct_cronacts(char *argres, int rw)
+{
+    struct jWriteControl jwc;
+    jwOpen(&jwc, argres, VAR_MAX_VALUE_LENGTH, JW_ARRAY, JW_COMPACT);
+    for (int idx = 0; idx < CRON_OBJECTS_NUMBER; idx++)
+    {
+        jwArr_string(&jwc, GetCronActionName(idx));
+    }
+    jwClose(&jwc);
+}
 
 static void funct_time(char *argres, int rw)
 {
@@ -36,14 +84,16 @@ static void funct_time(char *argres, int rw)
 const rest_var_t ApplicationVariables[] =
         {
                 /*FUNCTIONS*/
+                { 0, "cronrecs", &funct_cronrecs, VAR_FUNCT, R, 0, 0 },
+                { 0, "cronobjs", &funct_cronobjs, VAR_FUNCT, R, 0, 0 },
+                { 0, "cronacts", &funct_cronacts, VAR_FUNCT, R, 0, 0 },
+
                 { 0, "mytime", &funct_time, VAR_FUNCT, R, 0, 0 },
                 { 0, "myvar", &AppConfig.test, VAR_INT, R, 0, 0 }
 
         };
 
-
-
 void RegAppVariables(void)
 {
-    SetAppVars((rest_var_t*)ApplicationVariables, sizeof(ApplicationVariables) / sizeof(rest_var_t));
+    SetAppVars((rest_var_t*) ApplicationVariables, sizeof(ApplicationVariables) / sizeof(rest_var_t));
 }

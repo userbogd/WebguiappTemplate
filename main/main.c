70fab5f04b75eba4f1c3e752e7932731f0da8d26
bogdan <bogd@live.ru>
2023-10-22 10:28:32 +0200

    clean from old http handlers

--- main/main.c
+++ main/main.c
@@ -7,8 +7,6 @@
 #include "jobs.h"
 #include "AppConfiguration.h"
 
-int HTTPPrintCustom(httpd_req_t *req, char *buf, char *var, int arg);
-HTTP_IO_RESULT AfterPostHandlerCustom(httpd_req_t *req, const char *filename, char *PostData);
 void UserMQTTEventHndlr(int idx, void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data);
 void SaveUserConf();
 
@@ -21,8 +19,6 @@ const char my_context_data[] = "MyContextDataPassedIntoMQTTHandler";
 
 void app_main(void)
 {
-    regHTTPPrintCustom(&HTTPPrintCustom);
-    regAfterPostHandlerCustom(&AfterPostHandlerCustom);
     regUserEventHandler(&UserMQTTEventHndlr, (void*) my_context_data);
     regCustomSaveConf(&SaveUserConf);
     regCustomPayloadTypeHandler(&CustomPayloadHandler);

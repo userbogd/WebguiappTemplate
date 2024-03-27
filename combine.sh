0c42c5ef1bec8fb4cd2b18a22a7445b81d60a0db
bogd <bogd@live.ru>
2024-03-27 13:36:58 +0200

    release binary switched to esp32s3, components updated

--- combine.sh
+++ combine.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-esptool.py --chip esp32 merge_bin \
---output ./build/webguiapp_combined.bin \
-0x1000 ./build/bootloader/bootloader.bin \
+esptool.py --chip esp32s3 merge_bin \
+--output ./build/WebguiappTemplate_0000_hw1_ff.bin \
+0x0000 ./build/bootloader/bootloader.bin \
 0x8000 ./build/partition_table/partition-table.bin \
-0x11000 ./build/ota_data_initial.bin \
-0x20000 ./build/webguiapp_ref_implement.bin
+0x3C000 ./build/ota_data_initial.bin \
+0x40000 ./build/WebguiappTemplate.bin

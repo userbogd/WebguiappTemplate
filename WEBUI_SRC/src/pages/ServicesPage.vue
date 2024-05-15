a7444680497a669b6b4e6f3fe6a99e10ab243559
bogd <bogd@live.ru>
2024-05-15 15:51:24 +0200

    updated components cron and astro combine

--- WEBUI_SRC/src/pages/ServicesPage.vue
+++ WEBUI_SRC/src/pages/ServicesPage.vue
@@ -2,7 +2,6 @@
   <div class="cardholder">
     <SNTPCard></SNTPCard>
     <CronCard></CronCard>
-    <AstroTimerCard></AstroTimerCard>
     <MQTT1Card></MQTT1Card>
     <MQTT2Card></MQTT2Card>
   </div>
@@ -13,7 +12,7 @@ import SNTPCard from "components/webguicomp/cards/SNTPCard.vue";
 import MQTT1Card from "components/webguicomp/cards/MQTT1Card.vue";
 import MQTT2Card from "components/webguicomp/cards/MQTT2Card.vue";
 import CronCard from "src/components/webguicomp/cards/CronCard.vue";
-import AstroTimerCard from "src/components/webguicomp/cards/AstroTimerCard.vue";
+
 
 defineOptions({
   name: 'ServicesPage'

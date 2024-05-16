e7b69ba3d24b7e8bf4e5ade59d22b9aa4ec31aa5
bogd <bogd@live.ru>
2024-05-16 15:08:52 +0200

    astro timer implemented, components updated

--- WEBUI_SRC/src/pages/ServicesPage.vue
+++ WEBUI_SRC/src/pages/ServicesPage.vue
@@ -3,14 +3,12 @@
     <SNTPCard></SNTPCard>
     <CronCard></CronCard>
     <MQTT1Card></MQTT1Card>
-    <MQTT2Card></MQTT2Card>
   </div>
 </template>
 
 <script setup>
 import SNTPCard from "components/webguicomp/cards/SNTPCard.vue";
 import MQTT1Card from "components/webguicomp/cards/MQTT1Card.vue";
-import MQTT2Card from "components/webguicomp/cards/MQTT2Card.vue";
 import CronCard from "src/components/webguicomp/cards/CronCard.vue";
 
 

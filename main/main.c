30113c1bf96114d9b7a5d764e70206bd67b02ecd
bogd <bogd@live.ru>
2026-01-20 14:41:39 +0200

    cmake release argument added

--- main/main.c
+++ main/main.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <unistd.h>
+#include "esp_err.h"
+#include "freertos/idf_additions.h"
+#include "freertos/projdefs.h"
+#include "soc/gpio_num.h"
 #include "webguiapp.h"
 #include "AppConfiguration.h"
 #include "driver/gpio.h"
+#include "driver/dedic_gpio.h"
 
 void UserMQTTEventHndlr(int idx, void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data);
 void SaveUserConf();
@@ -24,22 +29,47 @@ void app_main(void)
     }
     ESP_ERROR_CHECK(InitAppConfig());
 
-	//mDNSstart();
-    LEDStripInit(CONFIG_LED_STRIP_GPIO);
-    
-    static bool BtnPressed = false;
+    // mDNSstart();
+    //LEDStripInit(CONFIG_LED_STRIP_GPIO);
+
+    gpio_set_direction(1, GPIO_MODE_INPUT_OUTPUT);
+    gpio_set_direction(2, GPIO_MODE_INPUT_OUTPUT);
+
+	while(1) {
+
+	vTaskDelay(pdMS_TO_TICKS(100));
+	gpio_set_level(GPIO_NUM_2, 1);
+
+	vTaskDelay(pdMS_TO_TICKS(100));
+	gpio_set_level(GPIO_NUM_2, 0);	
+
+    }
+
+
+	/*   
+    dedic_gpio_bundle_handle_t bundle;
+    int bidir_array[] = { 1, 2 };
+    dedic_gpio_bundle_config_t bidir_config = { .gpio_array = bidir_array, .array_size = 2, .flags = { .out_en = 1, .in_en = 1 } };
+
+    dedic_gpio_new_bundle(&bidir_config, &bundle);
+    static uint32_t data[1024];
     while (true)
     {
-        vTaskDelay(pdMS_TO_TICKS(100));
-        /*
-		if(gpio_get_level(GPIO_NUM_41) == 0 && !BtnPressed)
+        vTaskDelay(pdMS_TO_TICKS(10));
+        for (int i = 0; i < 1024; ++i)
         {
-            PublicTestMQTT(MQTT1);
-            PublicTestMQTT(MQTT2);
-            BtnPressed = true;
+            dedic_gpio_bundle_write(bundle, 1 << 0, 1 << 0);
+            dedic_gpio_bundle_write(bundle, 1 << 0, 0 << 0);
         }
-        if(gpio_get_level(GPIO_NUM_41) == 1 && BtnPressed)
-            BtnPressed = false;
-		*/
+
+        for (uint16_t i = 0; i < 1024; ++i)
+        {
+            dedic_gpio_bundle_write(bundle, 1 << 1, 1 << 1);
+            dedic_gpio_bundle_write(bundle, 1 << 1, 0 << 1);
+            *(data + i) = dedic_gpio_bundle_read_out(bundle);
+        }
     }
+	*/
+
+
 }
--- output_generator.sh
+++ output_generator.sh
@@ -1,18 +1,25 @@
  #!/bin/sh
-echo OUTPUT GENERATOR
+echo ****************Generate release artefacts************************
 VER=$(cat version.txt)
 PROJ_NAME=$1
 HW_REV=$2
+TARGET=$3
+echo "Artefacts version: ${VER}"
+echo "Device type: ${TARGET}"
+echo "Project name: ${PROJ_NAME}"
+echo "Hardware revision: ${HW_REV}"
 
-echo "VERSION ${VER}"
-echo "DEVICE ${IDF_TARGET}"
-echo "PROJECT NAME ${PROJ_NAME}"
-echo "HW revision ${HW_REV}"
+if [ "$TARGET" = "esp32" ]; then
+	BOOTLOADER_OFFSET=0x1000  
+else
+  	BOOTLOADER_OFFSET=0x0000
+fi
 
-esptool.py --chip esp32s3 merge_bin \
+esptool.py --chip ${TARGET} merge_bin \
 --output ./build/${PROJ_NAME}_${VER}_hw${HW_REV}_ff.bin \
-0x0000 ./build/bootloader/bootloader.bin \
+${BOOTLOADER_OFFSET} ./build/bootloader/bootloader.bin \
 0x8000 ./build/partition_table/partition-table.bin \
 0x3C000 ./build/ota_data_initial.bin \
 0x40000 ./build/${PROJ_NAME}.bin
-cp ./build/${PROJ_NAME}.bin ./build/${PROJ_NAME}_${VER}_hw${HW_REV}_ota.bin 
+
+cp ./build/${PROJ_NAME}.bin ./build/${PROJ_NAME}_${VER}_hw${HW_REV}_ota.bin
